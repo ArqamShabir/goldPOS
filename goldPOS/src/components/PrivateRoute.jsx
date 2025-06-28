964003241955847ef73baf9061cee7d04319e174
Muhammad Arqam <mrking6a@gmail.com>
2025-06-29 00:37:57 +0500

    Final clean commit with backend added

--- goldPOS/src/components/PrivateRoute.jsx
+++ goldPOS/src/components/PrivateRoute.jsx
@@ -14,7 +14,7 @@ const PrivateRoute = ({ children }) => {
       }
 
       try {
-        const res = await fetch('http://localhost:5000/api/verify-token', {
+        const res = await fetch('http://auricpos.com/api/verify-token', {
           headers: {
             Authorization: `Bearer ${token}`,
           },
